fb0da76dad210f904d266480235ce7396d77d9a2
Dmitry <necaevdimp@gmail.com>
2026-05-08 12:42:27 +0300

    feat(backend): add Spring Data JPA repositories

---
+++ backend/src/main/java/ru/mirea/repair/repository/RepairRequestRepository.java
@@ -0,0 +1,12 @@
+package ru.mirea.repair.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.mirea.repair.entity.RepairRequest;
+import ru.mirea.repair.entity.User;
+
+import java.util.List;
+
+public interface RepairRequestRepository extends JpaRepository<RepairRequest, Long> {
+    List<RepairRequest> findAllByUserOrderByCreatedAtDesc(User user);
+    List<RepairRequest> findAllByOrderByCreatedAtDesc();
+}
---
+++ backend/src/main/java/ru/mirea/repair/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package ru.mirea.repair.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.mirea.repair.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
