2d4d9da7635bbe410ed2dc5e10697bb62c83efbd
Dmitry <necaevdimp@gmail.com>
2026-05-08 12:43:15 +0300

    build(backend): add Dockerfile and smoke test

---
+++ backend/src/test/java/ru/mirea/repair/RepairRequestAppApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.mirea.repair;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class RepairRequestAppApplicationTests {
+    @Test
+    void smokeTest() {
+        assertTrue(true);
+    }
+}
